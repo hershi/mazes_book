8b235e9312b602aa802729ba29f26468a24cd603
Guy Hershenbaum <hershi@fb.com>
2019-05-26 20:59:44 -0700

    Initial version of binary tree

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -3,7 +3,6 @@ extern crate rand;
 use std::collections::HashSet;
 use std::iter;
 use std::fmt;
-use std::io::Write;
 
 #[derive(Hash, Eq, PartialEq, Debug, Clone)]
 struct Coordinates {
@@ -27,16 +26,24 @@ struct Cell {
     west: Option<Coordinates>,
 }
 
+fn validate_coordinates(pos: Coordinates, rows: isize, cols: isize) -> Option<Coordinates> {
+    if pos.row >= 0 && pos.row < rows && pos.col >=0 && pos.col < cols {
+        Some(pos)
+    } else {
+        None
+    }
+}
+
 impl Cell {
-    fn new(row: isize, col: isize) -> Cell {
+    fn new(row: isize, col: isize, rows: isize, cols: isize) -> Cell {
         let coordinates = Coordinates{row, col};
         Cell {
             coordinates,
             links: HashSet::new(),
-            north: Some(Coordinates{row: row-1, col}),
-            south:Some(Coordinates{row: row+1, col}),
-            east: Some(Coordinates{row , col: col+1}),
-            west: Some(Coordinates{row , col: col-1})}
+            north: validate_coordinates(Coordinates{row: row-1, col}, rows, cols),
+            south: validate_coordinates(Coordinates{row: row+1, col}, rows, cols),
+            east:  validate_coordinates(Coordinates{row , col: col+1}, rows, cols),
+            west:  validate_coordinates(Coordinates{row , col: col-1}, rows, cols)}
     }
 
     fn is_linked(&self, other_coordinates: &Option<Coordinates>) -> bool {
@@ -75,21 +82,20 @@ impl Grid {
         let mut grid = Vec::with_capacity(num_cells);
         for row in 0..rows {
             for col in 0..cols {
-                grid.push(Cell::new(row, col));
+                grid.push(Cell::new(row, col, rows, cols));
             }
         }
         Grid {grid,  rows, cols}
     }
 
-    //fn link(&mut self, cell1: Coordinates, cell2: Coordinates) {
-        //let cell1 = self.get_index(cell1);
-        //let cell2 = self.get_index(cell2);
-        //self.grid[cell1].insert(
-        //self.links.insert(cell.coordinates.clone());
-        //cell.links.insert(self.coordinates.clone());
-    //}
+    fn link(&mut self, pos1: Coordinates, pos2: Coordinates) {
+        let cell1 = self.get_index(&pos1).unwrap();
+        let cell2 = self.get_index(&pos2).unwrap();
+        self.grid[cell1].links.insert(pos2.clone());
+        self.grid[cell2].links.insert(pos1.clone());
+    }
 
-    fn get_index(&self, pos: Coordinates) -> Option<usize> {
+    fn get_index(&self, pos: &Coordinates) -> Option<usize> {
         if pos.row >= 0 && pos.row < self.rows && pos.col >=0 && pos.col < self.cols {
             Some((pos.row * self.cols + pos.col) as usize)
         } else {
@@ -97,7 +103,7 @@ impl Grid {
         }
     }
 
-    fn get(&self, pos: Coordinates) -> Option<&Cell> {
+    fn get(&self, pos: &Coordinates) -> Option<&Cell> {
         self.get_index(pos).map(|i| &self.grid[i])
     }
 
@@ -114,16 +120,16 @@ impl fmt::Display for Grid {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let line =
             iter::once("+")
-                .chain(iter::repeat("---+").take(self.cols as usize))
-                .collect::<Vec<_>>()
-                .join("");
+            .chain(iter::repeat("---+").take(self.cols as usize))
+            .collect::<Vec<_>>()
+            .join("");
         writeln!(f, "{}", line);
 
         for row in 0..self.rows {
             let mut line1 = "|".to_string();
             let mut line2 = "+".to_string();
             for col in 0..self.cols {
-                let cell = self.get(Coordinates::new(row,col)).unwrap();
+                let cell = self.get(&Coordinates::new(row,col)).unwrap();
                 if cell.is_linked(&cell.east) {
                     line1.push_str("    ");
                 } else {
@@ -144,34 +150,39 @@ impl fmt::Display for Grid {
     }
 }
 
-//fn binary_tree(grid: &mut Grid) {
-    //for cell in grid.grid.iter_mut() {
-        //if cell.north.is_none() && cell.east.is_none() { continue; }
+fn binary_tree_decide_link(cell: Option<&Cell>) -> Option<Coordinates> {
+    cell.map_or(None, |cell| {
+        let options = vec![cell.north.clone(), cell.east.clone()]
+            .into_iter()
+            .flat_map(|x| x)
+            .collect::<Vec<Coordinates>>();
 
-        //if cell.north.is_none() {
-            //let east = cell.east.unwrap();
-            //cell.link(grid.get_mut((east.row, east.col)).unwrap());
-            //continue;
-        //}
 
-        //if cell.east.is_none() {
-            //let north = cell.north.unwrap();
-            //cell.link(grid.get_mut((north.row, north.col)).unwrap());
-            //continue;
-        //}
+        if options.len() == 0 {
+            None
+        } else {
+            let idx = rand::random::<usize>() % options.len();
+            Some(options[idx].clone()) // why can't move?
+        }
+    })
+}
 
-        //if rand::random() {
-            //let north = cell.north.unwrap();
-            //cell.link(grid.get_mut((north.row, north.col)).unwrap());
-        //} else {
-            //let east = cell.east.unwrap();
-            //cell.link(grid.get_mut((east.row, east.col)).unwrap());
-        //}
-    //}
-//}
+fn binary_tree(grid: &mut Grid) {
+    for row in 0..grid.rows {
+        for col in 0..grid.cols {
+            let cell_pos = Coordinates::new(row, col);
+
+            let linked = binary_tree_decide_link(grid.get(&cell_pos));
+
+            if linked.is_some() {
+                grid.link(cell_pos, linked.unwrap());
+            }
+        }
+    }
+}
 
 fn main() {
     let mut grid = Grid::new(10,10);
-    println!("{:?}", grid.get(Coordinates::new(1,2)));
+    binary_tree(&mut grid);
     println!("{}", grid);
 }
